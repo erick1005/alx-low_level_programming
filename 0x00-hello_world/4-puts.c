b51d5843c793ab5a13e74866586dbe37cbc4ed86
erick1005 <erickmonari1@gmail.com>
2023-03-16 08:26:46 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
+/*displaying a text returns 0*/
 #include <stdio.h>
 
-/* displaying a text returns 0 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 
 }
