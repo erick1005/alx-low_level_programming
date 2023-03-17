654bbd89e17ddfc4177a1de7f74ae977c8fe07ff
erick1005 <erickmonari1@gmail.com>
2023-03-17 12:27:22 -0700

    cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - displays characters in the alphabet
+ *
+ * Return: returns zero
+ */
+int main(void)
+{
+        char str;
+
+        for (str = 'a'; str <= 'z'; str++)
+                putchar(str);
+	for (char str1 = 'A'; str1 <= 'Z'; str1++)
+		putchar(str1);
+        putchar('\n');
+        return (0);
+}
