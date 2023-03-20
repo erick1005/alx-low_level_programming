50d5ae921636db8c4f3ba22964b2b51875971853
erick1005 <erickmonari1@gmail.com>
2023-03-20 02:48:19 -0700

    pair combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main -entry of the programme
+ *
+ * Return: returns zero for success
+ *
+ */
+int main(void)
+{
+	int a, b, x, y;
+
+	for(a = 0; a <= 9; a++)
+	{
+		for(b = 0; b <= 9; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			putchar(' ');
+		}
+	}
+	for(x = 0; x <= 9; x++)
+	{
+		for(y = 0; y <= 9; y++)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
