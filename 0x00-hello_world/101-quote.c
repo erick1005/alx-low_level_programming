aa62126bb87d5885870ddfd71562e40a90e31d57
erick1005 <erickmonari1@gmail.com>
2023-03-19 23:02:25 -0700

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n";
 
-	fwrite(c, sizeof(char), sizeof(c), stderr);
+	fwrite(c,sizeof(char), sizeof(c), stderr);
 	return (1);
 }
