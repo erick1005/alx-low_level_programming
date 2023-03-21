c5ea8485f1cc0607220d3f53457f98e951439382
erick1005 <erickmonari1@gmail.com>
2023-03-21 08:11:46 -0700

    display code

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry of the programm
+ *
+ * Return: returns (0) for success
+ *
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
