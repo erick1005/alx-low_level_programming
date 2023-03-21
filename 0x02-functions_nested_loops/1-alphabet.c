1a3f72a3376d92d10c3680c125b47f61f7080c12
erick1005 <erickmonari1@gmail.com>
2023-03-21 12:00:09 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry of the code
+ *
+ * Return: 0 for success
+ */
+int print_alphabet();
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,16 @@
 #include <stdio.h>
 
 int print_putchar(void);
+int print_alphabet();
+
+int print_alphabet()
+{
+	char str[]={a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}
+
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
