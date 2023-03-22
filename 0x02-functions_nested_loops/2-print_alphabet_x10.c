26635d84d121946d1d705fa96ae7002915890065
erick1005 <erickmonari1@gmail.com>
2023-03-21 18:13:55 -0700

    multiple displays

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - displays alphabets 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	
+	char s;
+	for (s = 'a'; s <= 'z'; s++)
+	{
+	_putchar(s);
+	}
+	_putchar('\n');
+	}
+}
