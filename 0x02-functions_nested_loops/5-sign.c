09072761cabac8ea7c7a8482242b2b1df84a1748
erick1005 <erickmonari1@gmail.com>
2023-03-21 19:19:21 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <main.h>
+
+/**
+ * print_sign - prints the sighn of a number
+ * @n: number to be checked
+ * Return: 1 for +ve and -1 for -ve 0 none
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
