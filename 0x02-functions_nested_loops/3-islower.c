b72bf81e93672357f346ba3f78d1fe80865afeda
erick1005 <erickmonari1@gmail.com>
2023-03-21 18:38:28 -0700

    lower character check

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,13 @@
 
 /**
  * _islower - checks the character
- *
+ *@c: the item to be checked
  * Return:1 for success or 0 for failure
  */
 int _islower(int c)
 {
+	int c;
+
 	if (c >= 97 && c <= 122)
 	{
 	return (1);
