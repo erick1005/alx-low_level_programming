4c041339a70b0b725b512a18af3413f783eb715f
erick1005 <erickmonari1@gmail.com>
2023-03-23 11:31:57 -0700

    multiple characters

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - display a string of numbers multiple times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
