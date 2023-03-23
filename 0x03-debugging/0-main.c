a847990a2b986e2f8ac2a0ab71a2be42df9f8b2b
erick1005 <erickmonari1@gmail.com>
2023-03-23 00:43:03 -0700

    case 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -10,16 +10,8 @@ int main(void)
 {
 	int i;
 
-	i = 98;
-
-	if (i >= 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else
-	{
-		printf("%d is negative\n", i);
-	}
+	i = 0;
+	positive_or_negative(i);
 	return (0);
 
 }
