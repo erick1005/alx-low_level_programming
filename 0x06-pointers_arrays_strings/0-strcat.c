1b9cb152601463d55aa6bc366ee283ae073756ae
erick1005 <erickmonari1@gmail.com>
2023-03-29 02:52:51 -0700

    appends

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcat - appends two strings
+ * @dest: destination string
+ * @src: string to be copied to destination string
+ * Return: characters
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
