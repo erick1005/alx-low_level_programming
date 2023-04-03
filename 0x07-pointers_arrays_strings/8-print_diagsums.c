f69b9d12f009ef7ef9ea85c13021d5e4a97eb325
erick1005 <erickmonari1@gmail.com>
2023-04-03 14:14:30 -0700

    diagonal sum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - entry for the function
+ * @a: input array
+ * @size: array size
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, diagsum1 = 0, diagsum2 = 0;
+
+	for (i = 0; i <= (size * size); i = (i + size + 1))
+	{
+		diagsum1 += a[i];
+	}
+	for (j = size - 1; j <= (size * size) - size; j = j + size - 1)
+	{
+		diagsum2 += a[j];
+	}
+	printf("%d%d\n", diagsum1, diagsum2);
+}
