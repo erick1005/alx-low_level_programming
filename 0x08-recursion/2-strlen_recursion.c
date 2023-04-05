d483aed4a9a6507a878137998dcdbed607108873
erick1005 <erickmonari1@gmail.com>
2023-04-05 09:02:19 -0700

    size

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - entry point
+ * @s: string pointer
+ * Return: 0 for success
+ */
+int _strlen_recursion(char *s)
+{
+
+	int l = strlen(s);
+
+	return (l);
+}
