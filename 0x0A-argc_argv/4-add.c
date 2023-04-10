3ae2e83ae1bf8112f1dea2b603a26a480ce8f8ab
erick1005 <erickmonari1@gmail.com>
2023-04-10 11:02:30 -0700

    addittion

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - entry
+ * @argc: arguments count
+ * @argv: array of arguments
+ * Return: 0 for success always
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+	unsigned int num;
+	char *s;
+
+		if (argc > 1)
+		{
+			for (i = 1; i < argc; i++)
+			{
+			s = argv[i];
+
+			for (num = 0; num < strlen(s); num++)
+			{
+				if (s[num] < '0' || s[num] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(s);
+			s++;
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
