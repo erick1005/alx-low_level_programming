e2c7178ecaca34d1ec1b5647a8fabd25e4913fc4
erick1005 <erickmonari1@gmail.com>
2023-04-10 05:21:23 -0700

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @argc: argument count
+ * @argv: argument array
+ * Return: 0 for success
+ */
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
