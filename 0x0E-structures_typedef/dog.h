591440d83f0cc2d984b0baefbe1d9aab0c685966
erick1005 <erickmonari1@gmail.com>
2023-04-17 06:38:25 -0700

    first structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - object
+ * @name: input
+ * @age: input1
+ * @owner: input2
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
