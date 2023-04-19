d88ee50d6b531bc02711e07c845ad9945b7c4e33
erick1005 <erickmonari1@gmail.com>
2023-04-19 11:52:17 -0700

    number search

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function entry
+ * @array: array of elements
+ * @size: array size
+ * @cmp: action
+ * Return: integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+	return (-1);
+}
