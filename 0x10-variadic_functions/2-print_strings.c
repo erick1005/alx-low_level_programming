0a894ec18c28199e4f5fe0da0fa58b77c1c819a5
erick1005 <erickmonari1@gmail.com>
2023-04-20 13:27:23 -0700

    string display

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,11 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (separator == NULL)
-	{
-
-	}
-	else
+	if (separator != NULL && n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
