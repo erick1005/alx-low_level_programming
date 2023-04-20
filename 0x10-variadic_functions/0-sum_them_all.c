73b105c4112ebb23b0c2037be0aed51c763fcebd
erick1005 <erickmonari1@gmail.com>
2023-04-20 09:51:41 -0700

    first commit of the day

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - function entry
+ * @n: parameters to be added
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list args;
+
+	va_start(args, n);
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(args, int);
+		}
+	}
+	va_end(args);
+	return (sum);
+}
