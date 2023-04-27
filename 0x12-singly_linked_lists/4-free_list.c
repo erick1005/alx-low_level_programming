f4a65ede19a8c4a792c80f0c276ee846549a818b
erick1005 <erickmonari1@gmail.com>
2023-04-27 15:10:35 -0700

    free node

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - function free memory
+ * @head: first node
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *node;
+
+	while (head != NULL)
+	{
+		node = head;
+		head = head->next;
+
+		free(node);
+	}
+}
