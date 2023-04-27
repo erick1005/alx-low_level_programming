bd3217fdb5b3a47d84f60aecc1933037d6d8a9cb
erick1005 <erickmonari1@gmail.com>
2023-04-27 10:06:20 -0700

    print all nodes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,21 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef LISTS_H
+#define LISTS_H
+#include <stddef.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: input string
+ * @len: size of the string
+ * @next: points to the next node
+ * Description: singly list.
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
